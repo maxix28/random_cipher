eed6b72ec9b7385a200a189c8062f367faa27a73
MaxPavliv <maxyxpavliv@gmail.com>
2023-07-24 21:53:39 +0300

    add possibility to use latin letter in secret code
    add more conditions to input menu

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,49 +1,102 @@
+
 package bullscows
-import kotlin.random.Random
+import java.util.Random
+fun isNumericToX(toCheck: String): Boolean {
+    return toCheck.toDoubleOrNull() != null
+}
+const val SYMBOLS = "0123456789abcdefghijklmnopqrstuvxywz"
 class chipher(){
     protected
+    val alphabetList = ('a'..'z').map { it.toString() }
     var secret_Code="9305"
     var attemp=""
     var bull=0
     var cows=0
     var turn = 1
+    var stop = false
     init{
         println("Please, enter the secret code's length")
         setCode()
-        while (true){
-            println("Turn $turn:")
-            readAttemt()
-            check()
-            if(  Grade()){
+        if(!stop) {
+            while (true) {
+                println("Turn $turn:")
+                readAttemt()
+                check()
+                if (Grade()) {
+                    break
+                }
+                turn++
+            }
+            println("Congratulations! You guessed the secret code.")
+        }
+    }
+    var digits=""
+    fun setCode():Unit{
+        stop=false
+        var code = ""
+        var digits=""
+        while (true) {
+            digits = readln()
+            if( !isNumericToX(digits)){
+                println("Error: \"$digits\" isn't a valid number.")
+                stop =true
                 break
             }
-            turn++
-        }
-        println("Congratulations! You guessed the secret code.")
-
-    }
-    fun setCode(){
-        var code = ""
-        while (true) {
-            var digits = readln().toInt()
-            if (digits > 10) {
-                println("Error: can't generate a secret number with a length of 11 because there aren't enough unique digits.")
+            if (digits.toInt() ==0) {
+                println("Error: 0 isn't a valid number.")
+                stop =true
                 break}
-            var rnd = 0L
-            while (code.length < digits) {
-                if (rnd < 1) rnd = System.nanoTime()
-                val digit = (rnd % 10).toString().also { rnd /= 10 }
-                if (code.isEmpty() && digit == "0" || digit in code) continue
-                code += digit
-            }
+            if (digits.toInt() > 36) {
+
+                println("Error: maximum number of possible symbols in the code is 36 (0-9, a-z).")
+                stop =true
+                break}
             break
         }
-        secret_Code= code
-        println("Okay, let's start a game!")
+        if(!stop){
+            var symbol=0
+            sembol@ while(true){
+
+                println("Input the number of possible symbols in the code:")
+                symbol= readln().toInt()
+                if(symbol.toInt()<digits.toInt()){
+                    println("Error: it's not possible to generate a code with a length of ${digits} with $symbol unique symbols.")
+                    stop =true
+                    break@sembol
+                }
+                if(symbol>36){
+                    println("Error: maximum number of possible symbols in the code is 36 (0-9, a-z).")
+                    stop =true
+                    break@sembol
+
+                    break}
+
+                secret_Code=randomNumber(digits.toInt(),symbol)
+                println("The secret is prepared: ${"*".repeat(secret_Code.length)} (0-9, a-${alphabetList[Math.abs(symbol-11)]}).")
+
+                println("Okay, let's start a game!")
+                break@sembol
+            }
+        }
     }
     fun readAttemt(){
         attemp= readln()
 
+    }
+    fun randomNumber(num: Int, symbols: Int): String {
+        var secret = ""
+        var number=0
+        while (secret.length < num) {
+
+            if(symbols<10){ number = Random().nextInt(symbols)}
+            else number = Random().nextInt(symbols)
+            val symbol = SYMBOLS[number]
+
+            if (symbol !in secret) {
+                secret += symbol
+            }
+        }
+        return secret
     }
     fun check(){
 
